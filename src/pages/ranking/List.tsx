5472f69bbac8e509a00745617adaf9f6c6bc17fc
Bran <june20516@gmail.com>
2023-04-13 15:45:35 +0900

    랭킹 리스트 데이터 가져오기

--- src/pages/ranking/List.tsx
+++ src/pages/ranking/List.tsx
@@ -1,13 +1,82 @@
+import {
+  ComicRankApiSuccessResponse,
+  ComicRankItem,
+  isComicRankApiSuccessResponse,
+} from '../../types';
+
+import { ReactNode, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { get } from '../../lib/api';
+
 import { ListContainer } from '../../components/styledComponents';
-import { ComicRankItem } from '../../types';
 import ListItem from './ListItem';
 
-export default function List({ comics }: { comics: ComicRankItem[] }) {
+export default function List({ genre }: { genre: string }) {
+  const [hasNext, setHasNext] = useState(true);
+  const [comics, setComics] = useState<ComicRankItem[]>([]);
+
+  const loadComicRankItems = useCallback(
+    async (page: number) => {
+      get({ path: `/api/comics/${genre}`, page: page })
+        .then(async response => {
+          const parsedResponse = await response.json();
+          if (isComicRankApiSuccessResponse(parsedResponse)) {
+            const comicRankApiSucessResponse: ComicRankApiSuccessResponse = parsedResponse;
+            setHasNext(parsedResponse.hasNext);
+            setComics(prev => [...prev, ...comicRankApiSucessResponse.data]);
+          }
+        })
+        .catch(error => console.log(error));
+    },
+    [genre],
+  );
+
   return (
     <ListContainer>
       {comics.map(comic => {
-        return <ListItem comic={comic} />;
+        return <ListItem key={comic.id} comic={comic} />;
       })}
+      <InfiniteLoader hasNext={hasNext} loadMore={loadComicRankItems} />
     </ListContainer>
+  );
+}
+
+function InfiniteLoader({
+  hasNext,
+  loadMore,
+  children,
+}: {
+  hasNext: boolean;
+  loadMore: (page: number) => void;
+  children?: ReactNode;
+}) {
+  const [page, setPage] = useState(0);
+  const loading = useRef(null);
+  const options = useMemo(() => {
+    return {
+      root: null,
+      rootMargin: '20px',
+      threshold: 0.8,
+    };
+  }, []);
+
+  const handleObserver = useCallback(async (entries: any) => {
+    const target = entries[0];
+    if (target.isIntersecting) setPage(prev => prev + 1);
+  }, []);
+
+  useEffect(() => {
+    loadMore(page);
+  }, [loadMore, page]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (loading.current && hasNext) observer.observe(loading.current);
+    return () => observer.disconnect();
+  }, [handleObserver, hasNext, options]);
+
+  return (
+    <div ref={loading} style={{ width: '100%', display: hasNext ? 'block' : 'none' }}>
+      {children ? children : 'Loading More..'}
+    </div>
   );
 }
--- src/pages/ranking/index.tsx
+++ src/pages/ranking/index.tsx
@@ -1,14 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { get } from '../../lib/api';
-import {
-  ComicRankApiSuccessResponse,
-  ComicRankItem,
-  isComicRankApiSuccessResponse,
-} from '../../types';
 import { useSetRecoilState } from 'recoil';
 import { pageTitleState } from '../../recoil/metaInfo/atoms';
 import List from './List';
+import { MobilePageHasHeader } from '../../layout/styledComponents';
 
 const DEFAULT_GENRE_KEY = 'romance';
 
@@ -21,33 +16,21 @@ export default function Ranking() {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
-  const genre = searchParams.get('genre');
-  const [comics, setComics] = useState([] as ComicRankItem[]);
-  const [page, setPage] = useState(0);
+  const genre = searchParams.get('genre') || '';
   const setTitle = useSetRecoilState(pageTitleState);
 
   useEffect(() => {
     const validGenre = PREPARED_GENRES.find(prepared_genre => prepared_genre.key === genre);
     if (!validGenre) {
       navigate({ pathname: '/ranking', search: `?genre=${DEFAULT_GENRE_KEY}` });
-      setPage(p => 1);
     } else {
       setTitle(`${validGenre.displayName} 장르 랭킹`);
-      setPage(p => p + 1);
     }
   }, [genre, navigate, setTitle]);
 
-  useEffect(() => {
-    get({ path: `/api/comics/${genre}`, page: page })
-      .then(async response => {
-        const parsedResponse = await response.json();
-        if (isComicRankApiSuccessResponse(parsedResponse)) {
-          const comicRankApiSucessResponse: ComicRankApiSuccessResponse = parsedResponse;
-          setComics(comicRankApiSucessResponse.data);
-        }
-      })
-      .catch(error => console.error(error));
-  }, [page, genre]);
-
-  return <List comics={comics} />;
+  return (
+    <MobilePageHasHeader>
+      <List genre={genre} />
+    </MobilePageHasHeader>
+  );
 }
