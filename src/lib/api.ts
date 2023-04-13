a57bd6dc97fe2dfc87ef7e3d327e8deb571d188d
Bran <june20516@gmail.com>
2023-04-13 15:26:49 +0900

    중복 요청 방어 로직

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,28 @@
 const API_URL = 'http://localhost:3333';
 
-const get = ({ path, page }: { path: string; page: number }) => {
+const requestsBeenFetching: string[] = [];
+
+const get = ({ path, page }: { path: string; page: number }): Promise<Response> => {
   const url = new URL(path, API_URL);
   url.searchParams.set('page', page.toString());
-  return fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+
+  return new Promise((resolve, reject) => {
+    if (requestsBeenFetching.includes(url.toString())) {
+      reject(new Error('Duplicate Request'));
+    } else {
+      requestsBeenFetching.push(url.toString());
+      return fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => resolve(response))
+        .catch(error => reject(error))
+        .finally(() =>
+          requestsBeenFetching.splice(requestsBeenFetching.indexOf(url.toString()), 1),
+        );
+    }
   });
 };
 
